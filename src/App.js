9cb023e40abe29d22f1ad980b1377ede052eeb24
Marco <mmmarcoys@gmail.com>
2023-01-23 20:14:56 +0900

    ff

--- src/App.js
+++ src/App.js
@@ -1,16 +1,65 @@
 import logo from './logo.svg';
 import './Reset.css';
 import './App.css';
-import Header from './components/Header.js';
 import Main from './components/Main';
 import Company from './components/Company';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import Footer from './components/Footer';
+import { useEffect, useState } from 'react';
+import $ from 'jquery';
 
 function App() {
+  let navigate = useNavigate();
+  let [ mobmenu, setMobmenu ] = useState(false);
+  let [ menusl, setMenusl ] = useState('');
+  let [ bgblack, setBgblack ] = useState('');
+
+  function mobOn() {
+    setMobmenu(true);
+  }
+  function mobOff() {
+    setMobmenu(false);
+  }
+
+  useEffect(() => {
+    if( mobmenu == true ) {
+      setMenusl('mob-menu-on');
+      setBgblack('bg-black-on');
+      console.log(mobmenu);
+    } else {
+      setMenusl('');
+      setBgblack('');
+      console.log(mobmenu);
+    }
+
+    return () => {
+    }
+  }, [mobmenu])
+
   return (
     <div className="App">
-      <Header />
+      <div className={'bg-black ' + bgblack}></div>
+      
+      <div className='header'>
+        <img src="https://marcoys.github.io/frea/img/logo_w.png" alt="로고" className='top-logo' style={{ cursor: 'pointer' }} onClick={() => { navigate('/')}}/>
+
+        <ul className='top-menu'>
+          <li onClick={() => { navigate('/company')}}><p>회사소개</p></li>
+          <li><p>제품정보</p></li>
+          <li><p>문의하기</p></li>
+        </ul>
+
+        <button style={{ position: 'absolute', right: 0, top: 0}} onClick={mobOn}>모바일 메뉴</button>
+
+        <div className={'mob-menu ' + menusl}>
+          <button onClick={mobOff}>버튼</button>
+          <ul>
+            <li onClick={() => { navigate('/company')}}><p>회사소개</p></li>
+            <li><p>{mobmenu}</p></li>
+            <li><p>문의하기</p></li>
+          </ul>
+        </div>
+      </div>
 
       <Routes>
         <Route path='/' element={<Main />} />
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function Main() {
-  let [ down, setDown ] = useState('');
-  let [ left, setLeft ] = useState('');
-  useEffect(() => {
-    let a = setTimeout(() => {
-      setDown('down-end');
-    }, 500)
-    let b = setTimeout(() => {
-      setLeft('left-end');
-    }, 1000)
 
-    return () => {
-      clearTimeout(a, b);
-    }
+  useEffect(() => {
+    AOS.init();
   }, [])
 
   return (
@@ -21,16 +13,39 @@ function Main() {
       <div className='video-box'>
         <video src="https://marcoys.github.io/frea/img/main-bg.mp4" type='video/mp4' loop={true} playsInline={true} muted={true} autoPlay={true}></video>
         <div className='text-box'>
-          <h4 className={'down ' + down}><span className='txt-org'>F</span>REA FA</h4>
-          <h5 className={'left ' + left}>FA SOLUTION NO.1</h5>
+          <h4 data-aos="fade-down" data-aos-delay="300"><span className='txt-org'>F</span>REA FA</h4>
+          <h5 data-aos="fade-right" data-aos-delay="500">FA SOLUTION NO.1</h5>
         </div>
         
       </div>
 
-      <div className='main-box1'>
+      <div className='main-box1' data-aos="fade-up">
         <h3><span className='txt-navy'>S</span>mart <span className='txt-navy'>F</span>actory <span className='txt-navy'>S</span>olution</h3>
 
         <p>프레아 에프에이는 제조산업의 생산성과 품질을 혁신하는 솔루션을 생산, 기술, 품질, 영업 업무 전반을 포괄하는 솔루션을 공급하고 있습니다.<br /> 고객사 니즈와 상황에 맞추어 최적의 솔루션 공급으로 고객사의 발전에 기여하고 있습니다.</p>
+      </div>
+
+      <div className='main-box2'>
+        <div className='prd-box'>
+          <div>
+            <img src="https://marcoys.github.io/frea/img/imgTMS.jpg" alt="TMS" />
+            <h5>TMS</h5>
+            <p>Thickness Measurement System</p>
+            <p>두꼐 측정 시스템</p>
+          </div>
+          <div>
+            <img src="https://marcoys.github.io/frea/img/ds.png" alt="PTS" />
+            <h5>PTS</h5>
+            <p>Product Tracking System</p>
+            <p>데이터 관리 시스템</p>
+          </div>
+          <div>
+            <img src="https://marcoys.github.io/frea/img/imgFVS.png" alt="FVS" />
+            <h5>FVS</h5>
+            <p>FREA Vision System</p>
+            <p>비전 검사기</p>
+          </div>
+        </div>
       </div>
       
     </div>
