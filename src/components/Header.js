675114400ca62b351d2bdc58b3d889059aa5bd5e
Marco <ysmarco12@naver.com>
2023-01-26 03:31:37 +0900

    header component

---
+++ src/components/Header.js
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import $ from "jquery";
+
+function Header() {
+  let navigate = useNavigate();
+  let [mobmenu, setMobmenu] = useState(false);
+  let [menusl, setMenusl] = useState("");
+  let [bgblack, setBgblack] = useState("");
+
+  function mobOn() {
+    setMobmenu(true);
+    setMenusl("mob-menu-on");
+    setBgblack("bg-black-on");
+  }
+  function mobOff() {
+    setMobmenu(false);
+    setMenusl("");
+    setBgblack("");
+  }
+
+  useEffect(() => {
+    if (mobmenu === true) {
+      $("html").click((e) => {
+        if ($(e.target).hasClass("bg-black-on")) {
+          mobOff();
+          console.log("mob-close");
+        }
+      });
+    }
+  }, [mobmenu]);
+
+  return (
+    <>
+      <div className={"bg-black " + bgblack}></div>
+      <div className="header">
+        <img
+          src="https://marcoys.github.io/frea/img/logo_w.png"
+          alt="로고"
+          className="top-logo"
+          style={{ cursor: "pointer" }}
+          onClick={() => {
+            navigate("/");
+          }}
+        />
+
+        <ul className="top-menu">
+          <li
+            onClick={() => {
+              navigate("/company");
+            }}
+          >
+            <p>회사소개</p>
+          </li>
+          <li>
+            <p>제품정보</p>
+          </li>
+          <li>
+            <p>문의하기</p>
+          </li>
+        </ul>
+
+        <a className="menu-trigger" href="#" onClick={mobOn}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </a>
+
+        <div className={"mob-menu " + menusl}>
+          <button onClick={mobOff}>버튼</button>
+          <ul>
+            <li
+              onClick={() => {
+                navigate("/company");
+              }}
+            >
+              <p>회사소개</p>
+            </li>
+            <li>
+              <p>{mobmenu}</p>
+            </li>
+            <li>
+              <p>문의하기</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
